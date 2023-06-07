093b526009bccfe1446ee406cae1492c891dc343
haonguyen99 <hao.nguyen@ucars.sg>
2023-06-07 11:15:39 +0700

    Implement header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,74 @@
+"use client";
+import Image from "next/image";
+import LogoImage from "@public/logo_image.svg";
+import "./Header.scss";
+import { Button, Drawer } from "antd";
+import { useState } from "react";
+import {
+  AlignRightOutlined,
+  CloseOutlined,
+  PhoneFilled,
+} from "@ant-design/icons";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className="header-section">
+      <div className="container">
+        <Image src={LogoImage} alt="Logo Image" />
+        <div className="navigate-section">
+          <a href="">ABOUT US</a>
+          <a href="">CONTACT US</a>
+          <a href="">DOWNLOAD CATALOG</a>
+        </div>
+        <Button className="menu-btn" type="text" onClick={showDrawer}>
+          <AlignRightOutlined />
+        </Button>
+        <Drawer
+          className="drawer-homepage"
+          closeIcon={
+            <div className="drawer-close-icon">
+              <p>CLOSE</p>
+              <CloseOutlined />
+            </div>
+          }
+          title={
+            <Image className="logo-image" src={LogoImage} alt="Logo Image" />
+          }
+          placement="right"
+          onClose={onClose}
+          open={open}
+          width={"100%"}
+        >
+          <div className="navigate-section">
+            <a href="">ABOUT US</a>
+            <a href="">CONTACT US</a>
+            <a href="">DOWNLOAD CATALOG</a>
+          </div>
+          <div className="contact-rights-section">
+            <div className="contact-section">
+              <div className="common-row">
+                <PhoneFilled />
+                <p>+848-3-123-999</p>
+              </div>
+              <div className="common-row">
+                <PhoneFilled />
+                <p>+848-3-321-999</p>
+              </div>
+            </div>
+            <p className="rights">© GRANFORD 2023. All Rights Reserved.</p>
+          </div>
+        </Drawer>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import { Manrope, Roboto } from "next/font/google";
 import "antd/dist/reset.css";
 import "./styles/globals.scss";
+import Header from "./components/Header/Header";
 
 const manrope = Manrope({
   subsets: ["latin"],
+  variable: "--font-inter",
   weight: ["400", "500", "600", "800"],
 });
 
 const roboto = Roboto({
   subsets: ["latin"],
+  variable: "--font-roboto",
   weight: ["300", "400", "500", "700"],
 });
 
@@ -19,7 +22,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${manrope.className} ${roboto.className}`}>
+      <body className={`${manrope.className} ${roboto.variable}`}>
+        <Header />
         {children}
       </body>
     </html>
